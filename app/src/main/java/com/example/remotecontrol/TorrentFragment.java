5ef2552b3321ca4c7f301c930de2d6c4cc06cbcc
Co6oJIb <co6ojib@gmail.com>
2015-09-19 02:55:49 +0300

    Added rows to table

--- app/src/main/java/com/example/remotecontrol/TorrentFragment.java
+++ app/src/main/java/com/example/remotecontrol/TorrentFragment.java
@@ -1,8 +1,12 @@
 package com.example.remotecontrol;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +22,11 @@ public class TorrentFragment extends Fragment {
     private FragmentActivity faActivity;
     private TableLayout myTable;
 
+    public static int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,9 +40,19 @@ public class TorrentFragment extends Fragment {
             TextView myTextView1 = new TextView(getActivity());
             myTextView1.setText("Text " + i);
             TableRow row1 = new TableRow(getActivity());
-            row1.addView(myTextView1);
+            row1.setId(1000 + i);
+            RelativeLayout myRLayout = new RelativeLayout(getActivity());
+            myRLayout.addView(myTextView1);
+            RelativeLayout.LayoutParams rLParams = (RelativeLayout.LayoutParams)myTextView1.getLayoutParams();
+            rLParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            myTextView1.setLayoutParams(rLParams);
+            myRLayout.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, dpToPx(40)));
+            row1.addView(myRLayout);
             View mySeparator = new View(getActivity());
-            mySeparator.setBackgroundColor(0xFFC3C3C3);
+            mySeparator.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, dpToPx(1)));
+            mySeparator.setBackgroundColor(Color.argb(255, 195, 195, 195));
+            mySeparator.setId(1000 + i);
+
             myTable.addView(row1);
             myTable.addView(mySeparator);
         }
